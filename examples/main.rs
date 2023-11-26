eb021837703c58da2732ba220449f2f01ff7f006
Matt Hunzinger <matthunz2@gmail.com>
2023-11-26 03:52:19 -0500

    Initial Commit

---
+++ examples/main.rs
@@ -0,0 +1,93 @@
+use web_slinger::{Element, HtmlView};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+#[tokio::main]
+async fn main() -> wry::Result<()> {
+    let event_loop = EventLoop::new().unwrap();
+    let window =
+        WindowBuilder::new()
+            .with_inner_size(LogicalSize::new(800, 800))
+            .build(&event_loop)
+            .unwrap();
+
+    let (document, mut html_view) = HtmlView::new(&window).unwrap();
+    tokio::spawn(async move {
+        let node = document.create_text_node("Hello World!").await;
+        document.append_child(Element::body(), node).await;
+    });
+
+    #[cfg(any(
+        target_os = "linux",
+        target_os = "dragonfly",
+        target_os = "freebsd",
+        target_os = "netbsd",
+        target_os = "openbsd",
+    ))]
+    {
+        use gtk::prelude::DisplayExtManual;
+
+        gtk::init().unwrap();
+        if gtk::gdk::Display::default().unwrap().backend().is_wayland() {
+            panic!("This example doesn't support wayland!");
+        }
+
+        // we need to ignore this error here otherwise it will be catched by winit and will be
+        // make the example crash
+        winit::platform::x11::register_xlib_error_hook(Box::new(|_display, error| {
+            let error = error as *mut x11_dl::xlib::XErrorEvent;
+            (unsafe { (*error).error_code }) == 170
+        }));
+    }
+
+    event_loop
+        .run(move |event, evl| {
+            evl.set_control_flow(ControlFlow::Poll);
+
+            #[cfg(any(
+                target_os = "linux",
+                target_os = "dragonfly",
+                target_os = "freebsd",
+                target_os = "netbsd",
+                target_os = "openbsd",
+            ))]
+            while gtk::events_pending() {
+                gtk::main_iteration_do(false);
+            }
+
+            html_view.poll();
+
+            match event {
+                #[cfg(any(
+                    target_os = "linux",
+                    target_os = "dragonfly",
+                    target_os = "freebsd",
+                    target_os = "netbsd",
+                    target_os = "openbsd",
+                ))]
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    _webview.set_bounds(wry::Rect {
+                        x: 0,
+                        y: 0,
+                        width: size.width,
+                        height: size.height,
+                    });
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => evl.exit(),
+                _ => {}
+            }
+        })
+        .unwrap();
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,319 @@
+use serde::Deserialize;
+use std::borrow::Cow;
+use tokio::sync::{mpsc, oneshot};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use wry::{raw_window_handle::HasRawWindowHandle, WebView, WebViewBuilder};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Element {
+    id: u64,
+}
+
+impl Element {
+    pub const fn body() -> Self {
+        Self { id: 0 }
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "kind", content = "data")]
+enum Message {
+    CreateNode { id: u64 },
+    AppendChild,
+}
+
+enum Request {
+    AppendChild {
+        parent_id: u64,
+        child_id: u64,
+        tx: Option<oneshot::Sender<()>>,
+    },
+    CreateElement {
+        name: Cow<'static, str>,
+        tx: Option<oneshot::Sender<u64>>,
+    },
+    CreateTextElement {
+        content: Cow<'static, str>,
+        tx: Option<oneshot::Sender<u64>>,
+    },
+}
+
+pub struct Document {
+    tx: mpsc::UnboundedSender<Request>,
+}
+
+impl Document {
+    pub async fn append_child(&self, parent: Element, child: Element) {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(Request::AppendChild {
+                parent_id: parent.id,
+                child_id: child.id,
+                tx: Some(tx),
+            })
+            .unwrap();
+        rx.await.unwrap()
+    }
+
+    pub async fn create_element(&self, name: impl Into<Cow<'static, str>>) -> Element {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(Request::CreateElement {
+                name: name.into(),
+                tx: Some(tx),
+            })
+            .unwrap();
+        let id = rx.await.unwrap();
+        Element { id }
+    }
+
+    pub async fn create_text_node(&self, content: impl Into<Cow<'static, str>>) -> Element {
+        let (tx, rx) = oneshot::channel();
+        self.tx
+            .send(Request::CreateTextElement {
+                content: content.into(),
+                tx: Some(tx),
+            })
+            .unwrap();
+        let id = rx.await.unwrap();
+        Element { id }
+    }
+}
+
+pub struct HtmlView {
+    web_view: WebView,
+    rx: mpsc::UnboundedReceiver<Message>,
+    req_rx: mpsc::UnboundedReceiver<Request>,
+    pending: Option<Request>,
+}
+
+impl HtmlView {
+    pub fn new(window: &impl HasRawWindowHandle) -> wry::Result<(Document, Self)> {
+        let builder = WebViewBuilder::new(&window);
+
+        let (tx, rx) = mpsc::unbounded_channel();
+
+        let web_view =
+            builder
+                .with_html("<html><body></body></html>")?
+                .with_ipc_handler(move |s| {
+                    let msg = serde_json::from_str(&s).unwrap();
+                    tx.send(msg).unwrap();
+                })
+                .build()?;
+
+        web_view
+            .evaluate_script(&format!(
+                r#"
+                window.webSlinger = {{
+                    elements: {{
+                        0: document.body
+                    }},
+                    nextId: 1
+                }}
+            "#
+            ))
+            .unwrap();
+
+        let (req_tx, req_rx) = mpsc::unbounded_channel();
+
+        Ok((
+            Document { tx: req_tx },
+            Self {
+                web_view,
+                rx,
+                req_rx,
+                pending: None,
+            },
+        ))
+    }
+
+    pub fn poll(&mut self) {
+        if let Some(ref mut req) = self.pending {
+            match req {
+                Request::AppendChild {
+                    parent_id: _,
+                    child_id: _,
+                    tx,
+                } => {
+                    if let Ok(msg) = self.rx.try_recv() {
+                        match msg {
+                            Message::AppendChild => {
+                                tx.take().unwrap().send(()).unwrap();
+                            }
+                            _ => todo!(),
+                        }
+
+                        self.pending = None;
+                    } else {
+                        return;
+                    }
+                }
+                Request::CreateElement { name: _, tx } => {
+                    if let Ok(msg) = self.rx.try_recv() {
+                        match msg {
+                            Message::CreateNode { id } => {
+                                tx.take().unwrap().send(id).unwrap();
+                            }
+                            _ => todo!(),
+                        }
+
+                        self.pending = None;
+                    } else {
+                        return;
+                    }
+                }
+                Request::CreateTextElement { content: _, tx } => {
+                    if let Ok(msg) = self.rx.try_recv() {
+                        match msg {
+                            Message::CreateNode { id } => {
+                                tx.take().unwrap().send(id).unwrap();
+                            }
+                            _ => todo!(),
+                        }
+
+                        self.pending = None;
+                    } else {
+                        return;
+                    }
+                }
+            }
+        }
+
+        if let Ok(req) = self.req_rx.try_recv() {
+            match &req {
+                Request::AppendChild {
+                    parent_id,
+                    child_id,
+                    tx: _,
+                } => {
+                    self.web_view
+                        .evaluate_script(&format!(
+                            r#"
+                            var parent = window.webSlinger.elements[{parent_id}];
+                            var child = window.webSlinger.elements[{child_id}];
+                            parent.appendChild(child);
+
+                            window.ipc.postMessage(JSON.stringify({{ kind: "AppendChild" }}));
+                        "#
+                        ))
+                        .unwrap();
+                }
+                Request::CreateElement { name, tx: _ } => {
+                    self.web_view
+                    .evaluate_script(&format!(
+                        r#"
+                            let element = document.createElement("{name}");
+                            
+                            var id = window.webSlinger.nextId;
+                            window.webSlinger.nextId += 1;
+        
+                            window.webSlinger.elements[id] = element;
+                            window.ipc.postMessage(JSON.stringify({{ kind: "CreateNode", data: {{ id: id }} }}));
+                        "#
+                    ))
+                    .unwrap();
+                }
+                Request::CreateTextElement { content, tx: _ } => {
+                    self.web_view
+                    .evaluate_script(&format!(
+                        r#"
+                            let node = document.createTextNode("{content}");
+                            
+                            var id = window.webSlinger.nextId;
+                            window.webSlinger.nextId += 1;
+        
+                            window.webSlinger.elements[id] = node;
+                            window.ipc.postMessage(JSON.stringify({{ kind: "CreateNode", data: {{ id: id }} }}));
+                        "#
+                    ))
+                    .unwrap();
+                }
+            }
+            self.pending = Some(req);
+        }
+    }
+
+    pub fn run(mut self) -> wry::Result<()> {
+        #[cfg(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        ))]
+        {
+            use gtk::prelude::DisplayExtManual;
+
+            gtk::init().unwrap();
+            if gtk::gdk::Display::default().unwrap().backend().is_wayland() {
+                panic!("This example doesn't support wayland!");
+            }
+
+            // we need to ignore this error here otherwise it will be catched by winit and will be
+            // make the example crash
+            winit::platform::x11::register_xlib_error_hook(Box::new(|_display, error| {
+                let error = error as *mut x11_dl::xlib::XErrorEvent;
+                (unsafe { (*error).error_code }) == 170
+            }));
+        }
+
+        let event_loop = EventLoop::new().unwrap();
+        let _window = WindowBuilder::new()
+            .with_inner_size(LogicalSize::new(800, 800))
+            .build(&event_loop)
+            .unwrap();
+
+        event_loop
+            .run(move |event, evl| {
+                evl.set_control_flow(ControlFlow::Poll);
+
+                #[cfg(any(
+                    target_os = "linux",
+                    target_os = "dragonfly",
+                    target_os = "freebsd",
+                    target_os = "netbsd",
+                    target_os = "openbsd",
+                ))]
+                while gtk::events_pending() {
+                    gtk::main_iteration_do(false);
+                }
+
+                self.poll();
+
+                match event {
+                    #[cfg(any(
+                        target_os = "linux",
+                        target_os = "dragonfly",
+                        target_os = "freebsd",
+                        target_os = "netbsd",
+                        target_os = "openbsd",
+                    ))]
+                    Event::WindowEvent {
+                        event: WindowEvent::Resized(size),
+                        ..
+                    } => {
+                        _webview.set_bounds(wry::Rect {
+                            x: 0,
+                            y: 0,
+                            width: size.width,
+                            height: size.height,
+                        });
+                    }
+                    Event::WindowEvent {
+                        event: WindowEvent::CloseRequested,
+                        ..
+                    } => evl.exit(),
+                    _ => {}
+                }
+            })
+            .unwrap();
+
+        Ok(())
+    }
+}
